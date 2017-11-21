17a5e743102da5a263820e4e654b46eb46462485
Ghislain de Juvigny <gdjuvigny@gmail.com>
2017-11-21 12:58:21 +0100

    fix db auth

--- src/config/dev.js
+++ src/config/dev.js
@@ -1,7 +1,7 @@
 // Fichier de configuration lu lorsque vous lancez l'application en local sur votre ordinateur
 
 export default {
-  bddUri: 'ds135384.mlab.com:35384/hackx',
+  bddUri: 'ds113906.mlab.com:13906/trashless-db',
 }
 
 //ds113906.mlab.com:13906/trashless-db
--- src/config/prod.js
+++ src/config/prod.js
@@ -1,7 +1,7 @@
 // Fichier de configuration lu lorsque vous lancez l'application sur votre serveur Heroku
 
 export default {
-  bddUri: 'ds135384.mlab.com:35384/hackx',
+  bddUri: 'ds113906.mlab.com:13906/trashless-db',
 }
 
 //ds113906.mlab.com:13906/trashless-db
